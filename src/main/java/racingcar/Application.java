b613924c38285a833f49718040701e065ebb7089
kyupid <iamscvv@gmail.com>
2021-07-26 18:23:46 +0900

    Init

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,35 @@
 package racingcar;
 
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+
+        String[] names = scanner.next().split(",");
+        int distanceOfLane = scanner.nextInt();
+
+        List<Car> cars = new ArrayList<>();
+
+        for (int i = 0; i < names.length; i++) {
+            cars.add(new Car(names[i]));
+        }
+
+        for (int i = 0; i < distanceOfLane; i++) {
+                int randomNumber = RandomUtils.nextInt(0, 9);
+                Car car = cars.get(i);
+                String name = car.getName();
+                System.out.print(name + " : ");
+                if (randomNumber > 3) {
+                    System.out.println("-");
+                } else {
+                    break;
+                }
+        }
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,10 +3,22 @@ package racingcar;
 public class Car {
     private final String name;
     private int position = 0;
+    private int randomNum = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setRandomNum(int randomNum) {
+        this.randomNum = randomNum;
+    }
+
     // 추가 기능 구현
+    public void pushPedal() {
+        position++;
+    }
 }
